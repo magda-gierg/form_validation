f0848c0b5f79e65f21d1d2dd8bd55bb2574d43b7
magda-gierg <mjgiergiczny@gmail.com>
2018-07-02 12:15:46 +1200

    created form inputs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import Form from './Form'
+
+
+
+const App = ({children}) => (
+  <div className="container">
+    <Form />
+  </div>
+)
+
+export default App
